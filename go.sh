47a64970ba2fee96e4c93c418cc53d59b0cb85be
pseudotensor <pseudotensor@gmail.com>
2017-06-06 17:25:53 -0700

    Updated CMakelists so compiles properly for GPU and fixed dependency issues so make -j works properly

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
-rm -rf build ; mkdir build ; cd build  ; cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  -DPLUGIN_UPDATER_GPU=ON -DCUB_DIRECTORY="~/cub/" .. ; make -j &> make0.log ; make clean &> make1.log ; make -j &> make.log
-#rm -rf build ; mkdir build ; cd build  ; cmake -DPLUGIN_UPDATER_GPU=ON -DCUB_DIRECTORY="~/cub/" .. ; make -j &> make0.log ; make clean &> make1.log ; make -j &> make.log
+rm -rf build ; mkdir build ; cd build  ; cmake -DCMAKE_BUILD_TYPE=Debug  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  -DPLUGIN_UPDATER_GPU=ON .. ; make -j &> make0.log ; make clean &> make1.log ; make -j &> make.log
+#rm -rf build ; mkdir build ; cd build  ; cmake -DPLUGIN_UPDATER_GPU=ON .. ; make -j &> make0.log ; make clean &> make1.log ; make -j &> make.log
 cd ../python-package
 python setup.py install --user
 python3 setup.py install --user
